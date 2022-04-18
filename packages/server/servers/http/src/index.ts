e5576ad913c3760d0210e059a7eb13439d49e2fc
Nikita <nikita.my.email@gmail.com>
2022-04-18 13:19:10 +0300

    describe base API

--- packages/server/servers/http/src/index.ts
+++ packages/server/servers/http/src/index.ts
@@ -1,11 +1,74 @@
-import http from 'http';
+import http, { IncomingMessage } from 'http';
 
-export default (port: number, hostname: string) => {
+import { TServer, Routing, Methods } from './types';
+
+//TODO: parser url
+//TODO: implement reading get parametrs
+//TODO: add handling errors
+//TODO: implement check cookies
+//TODO: implement response to client
+
+type TMethodsAndRoutes = {
+  method: Methods;
+  route: Routing;
+};
+
+const getRouteAndMethod = (pathname: string): TMethodsAndRoutes => {
+  const [route, method] = pathname.substring(1).split('/');
+
+  const currentRoute = route as Routing;
+  const currentMethod = method as Methods;
+
+  if (!Object.values(Routing).includes(currentRoute)) throw new Error('Route Not found');
+  if (!Object.values(Methods).includes(currentMethod)) throw new Error('Method Not found');
+
+  return {
+    route: currentRoute,
+    method: currentMethod,
+  };
+};
+
+const bodyParser = async (req: IncomingMessage) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  Buffer.concat(buffers).toString();
+
+  return Buffer.concat(buffers).toString();
+};
+
+export default ({ port, hostname, routing }: TServer) => {
   http
-    .createServer((req, res) => {
-      res.statusCode = 200;
+    .createServer(async (req, res) => {
+      if (!req?.url) return;
+
+      let body: any;
+
+      const { pathname, searchParams } = new URL(`http://${hostname}:${port}${req?.url}`);
+      const { route, method } = getRouteAndMethod(pathname);
+
+      const apiRoute = routing?.[route];
+
+      if (req.method !== 'GET') {
+        body = await bodyParser(req);
+      }
+
+      if (!apiRoute) return;
+
+      const handler = apiRoute[method];
+
+      if (!handler) return;
+
+      const response = await handler({ body, searchParams });
+
+      const parsedResponse = JSON.stringify(response);
+
+      res.statusCode = response?.code;
       res.setHeader('Content-Type', 'text/plain');
-      res.end('Hellasdadso World');
+      res.end(parsedResponse);
     })
     .listen(port, hostname, () => {
       console.log(`Server runnisssssng at http://${hostname}:${port}/`);
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,19 @@
 import server from '@server/http';
+import { TServerRouting, CODES } from '../servers/http/src/types'; //TMP
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-server(port, hostname);
+const routing: TServerRouting = {
+  user: {
+    createUser: async (args) => ({
+      code: CODES.OK,
+      data: { ...args, params: args.searchParams?.getAll('id') },
+    }),
+    getUser: async (args) => ({
+      code: CODES.BAD_REQUEST,
+    }),
+  },
+};
+
+server({ port, hostname, routing });
